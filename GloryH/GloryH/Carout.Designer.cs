d3e843d4e5f2d2f2ce92c16c99d189713f7d7c89
佳蔚 唐 <1277569558@qq.com>
2023-06-06 14:26:41 +0800

    Initial commit

---
+++ GloryH/GloryH/Carout.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace GloryH
+{
+    partial class Carout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.parking_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.card_value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.car_number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.car_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.begin_time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.state = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.verify1 = new System.Windows.Forms.TextBox();
+            this.type1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.button2.Location = new System.Drawing.Point(676, 491);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 48);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "驶出并计费";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.button1.Location = new System.Drawing.Point(1057, 209);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 32);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "查询";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.label4.Location = new System.Drawing.Point(609, 291);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 25);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "对应信息如下：";
+            this.label4.Visible = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.parking_id,
+            this.card_value,
+            this.car_number,
+            this.car_name,
+            this.begin_time,
+            this.state});
+            this.dataGridView1.Location = new System.Drawing.Point(353, 320);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(857, 163);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.Visible = false;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "编号";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.Width = 125;
+            // 
+            // parking_id
+            // 
+            this.parking_id.DataPropertyName = "parking_id";
+            this.parking_id.HeaderText = "车位编号";
+            this.parking_id.MinimumWidth = 6;
+            this.parking_id.Name = "parking_id";
+            this.parking_id.Width = 125;
+            // 
+            // card_value
+            // 
+            this.card_value.DataPropertyName = "card_value";
+            this.card_value.HeaderText = "停车证号";
+            this.card_value.MinimumWidth = 6;
+            this.card_value.Name = "card_value";
+            this.card_value.Width = 125;
+            // 
+            // car_number
+            // 
+            this.car_number.DataPropertyName = "car_number";
+            this.car_number.HeaderText = "车牌号";
+            this.car_number.MinimumWidth = 6;
+            this.car_number.Name = "car_number";
+            this.car_number.Width = 125;
+            // 
+            // car_name
+            // 
+            this.car_name.DataPropertyName = "car_user";
+            this.car_name.HeaderText = "车主姓名";
+            this.car_name.MinimumWidth = 6;
+            this.car_name.Name = "car_name";
+            this.car_name.Width = 125;
+            // 
+            // begin_time
+            // 
+            this.begin_time.DataPropertyName = "begin_time";
+            this.begin_time.HeaderText = "停入时间";
+            this.begin_time.MinimumWidth = 6;
+            this.begin_time.Name = "begin_time";
+            this.begin_time.Width = 125;
+            // 
+            // state
+            // 
+            this.state.DataPropertyName = "state";
+            this.state.HeaderText = "车位状态";
+            this.state.MinimumWidth = 6;
+            this.state.Name = "state";
+            this.state.Width = 125;
+            // 
+            // verify1
+            // 
+            this.verify1.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.verify1.Location = new System.Drawing.Point(785, 204);
+            this.verify1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.verify1.Name = "verify1";
+            this.verify1.Size = new System.Drawing.Size(208, 35);
+            this.verify1.TabIndex = 14;
+            this.verify1.TextChanged += new System.EventHandler(this.Verify1_TextChanged);
+            // 
+            // type1
+            // 
+            this.type1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.type1.Font = new System.Drawing.Font("宋体", 15F);
+            this.type1.FormattingEnabled = true;
+            this.type1.Items.AddRange(new object[] {
+            "车牌号",
+            "证件号"});
+            this.type1.Location = new System.Drawing.Point(635, 206);
+            this.type1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.type1.Name = "type1";
+            this.type1.Size = new System.Drawing.Size(115, 33);
+            this.type1.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.label2.Location = new System.Drawing.Point(488, 211);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 25);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "信息验证：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("黑体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(608, 85);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 37);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "车辆驶出";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // Carout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1579, 795);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.verify1);
+            this.Controls.Add(this.type1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Carout";
+            this.Text = "车辆驶出";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox verify1;
+        private System.Windows.Forms.ComboBox type1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn parking_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn card_value;
+        private System.Windows.Forms.DataGridViewTextBoxColumn car_number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn car_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn begin_time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn state;
+    }
+}
---
+++ GloryH/GloryH/Cost.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace GloryH
+{
+    partial class Cost
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.hour1 = new System.Windows.Forms.TextBox();
+            this.month1 = new System.Windows.Forms.TextBox();
+            this.day1 = new System.Windows.Forms.TextBox();
+            this.year1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("黑体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(551, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 37);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "停车场收费标准";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.label3.Location = new System.Drawing.Point(420, 212);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 25);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "每小时收费：";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.label4.Location = new System.Drawing.Point(447, 435);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 25);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "每年收费：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.label5.Location = new System.Drawing.Point(447, 361);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(137, 25);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "每月收费：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.label6.Location = new System.Drawing.Point(447, 291);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 25);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "每天收费：";
+            // 
+            // hour1
+            // 
+            this.hour1.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.hour1.Location = new System.Drawing.Point(625, 209);
+            this.hour1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.hour1.Name = "hour1";
+            this.hour1.Size = new System.Drawing.Size(147, 35);
+            this.hour1.TabIndex = 32;
+            // 
+            // month1
+            // 
+            this.month1.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.month1.Location = new System.Drawing.Point(625, 349);
+            this.month1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.month1.Name = "month1";
+            this.month1.Size = new System.Drawing.Size(147, 35);
+            this.month1.TabIndex = 33;
+            // 
+            // day1
+            // 
+            this.day1.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.day1.Location = new System.Drawing.Point(625, 288);
+            this.day1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.day1.Name = "day1";
+            this.day1.Size = new System.Drawing.Size(147, 35);
+            this.day1.TabIndex = 34;
+            // 
+            // year1
+            // 
+            this.year1.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.year1.Location = new System.Drawing.Point(625, 422);
+            this.year1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.year1.Name = "year1";
+            this.year1.Size = new System.Drawing.Size(147, 35);
+            this.year1.TabIndex = 35;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.label7.Location = new System.Drawing.Point(819, 212);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 25);
+            this.label7.TabIndex = 36;
+            this.label7.Text = "元";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.label8.Location = new System.Drawing.Point(819, 435);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 25);
+            this.label8.TabIndex = 37;
+            this.label8.Text = "元";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.label9.Location = new System.Drawing.Point(819, 361);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 25);
+            this.label9.TabIndex = 38;
+            this.label9.Text = "元";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.label10.Location = new System.Drawing.Point(819, 291);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(37, 25);
+            this.label10.TabIndex = 39;
+            this.label10.Text = "元";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 14.5F);
+            this.button1.Location = new System.Drawing.Point(557, 554);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 40);
+            this.button1.TabIndex = 40;
+            this.button1.Text = "确认修改";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Cost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1579, 795);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.year1);
+            this.Controls.Add(this.day1);
+            this.Controls.Add(this.month1);
+            this.Controls.Add(this.hour1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Cost";
+            this.Text = "Cost";
+            this.Load += new System.EventHandler(this.Cost_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox hour1;
+        private System.Windows.Forms.TextBox month1;
+        private System.Windows.Forms.TextBox day1;
+        private System.Windows.Forms.TextBox year1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+    }
+}
